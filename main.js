8916fbbb7fcbbd75b0707a442298335ca72a5b41
Dylan <huanglinjay123@outlook.com>
2018-09-24 18:52:49 +0800

    写成3个状态类（current,enter,leave）

--- main.js
+++ main.js
@@ -1,37 +1,37 @@
+//初始化位置
+$('.images>img:nth-child(1)').addClass('current');
+$('.images>img:nth-child(2)').addClass('enter');
+$('.images>img:nth-child(3)').addClass('enter');
 
+//每2秒后添加相应类去移动图片
 setTimeout(() => {
-    $('.images>img:nth-child(1)').css({
-        transform:'translateX(-100%)'
-    })
-    $('.images>img:nth-child(2)').css({
-        transform:'translateX(-100%)'
-    })
-    $('.images>img:nth-child(1)').one('transitionend',function(a){
-        $(a.currentTarget).addClass('right').css({transform:'none'})
-    })
+    $('.images>img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend',function(a){
+            $(a.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $('.images>img:nth-child(2)').removeClass('enter').addClass('current');
 }, 2000);
 
 setTimeout(() => {
-    $('.images>img:nth-child(2)').css({
-        transform:'translateX(-200%)'
-    })
-    $('.images>img:nth-child(3)').css({
-        transform:'translateX(-100%)'
-    })
-    $('.images>img:nth-child(2)').one('transitionend',function(a){
-        $(a.currentTarget).addClass('right').css({transform:'none'})
-    })
+    $('.images>img:nth-child(2)').removeClass('current').addClass('leave')
+        .one('transitionend',function(a){
+            $(a.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $('.images>img:nth-child(3)').removeClass('enter').addClass('current');
 }, 4000);
 
 setTimeout(() => {
-    $('.images>img:nth-child(3)').css({
-        transform:'translateX(-200%)'
-    })
-    $('.images>img:nth-child(1)').css({
-        transform:'translateX(-100%)',
-        'z-index':'1'
-    })
-    $('.images>img:nth-child(3)').one('transitionend',function(a){
-        $(a.currentTarget).addClass('right').css({transform:'none'})
-    })
-}, 6000);  
+    $('.images>img:nth-child(3)').removeClass('current').addClass('leave')
+        .one('transitionend',function(a){
+            $(a.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $('.images>img:nth-child(1)').removeClass('enter').addClass('current');
+}, 6000);
+
+setTimeout(() => {
+    $('.images>img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend',function(a){
+            $(a.currentTarget).removeClass('leave').addClass('enter')
+        })
+    $('.images>img:nth-child(2)').removeClass('enter').addClass('current');
+}, 8000);
