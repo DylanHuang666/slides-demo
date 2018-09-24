cc742bcd19225a6680480f0467ea44b55f8c3c14
Dylan <huanglinjay123@outlook.com>
2018-09-24 19:30:59 +0800

    自动轮播

--- main.js
+++ main.js
@@ -4,34 +4,22 @@ $('.images>img:nth-child(2)').addClass('enter');
 $('.images>img:nth-child(3)').addClass('enter');
 
 //每2秒后添加相应类去移动图片
-setTimeout(() => {
-    $('.images>img:nth-child(1)').removeClass('current').addClass('leave')
+let n=1
+setInterval(() => {
+    $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
         .one('transitionend',function(a){
             $(a.currentTarget).removeClass('leave').addClass('enter')
         })
-    $('.images>img:nth-child(2)').removeClass('enter').addClass('current');
-}, 2000);
+    $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+    n+=1
+}, 2000)
 
-setTimeout(() => {
-    $('.images>img:nth-child(2)').removeClass('current').addClass('leave')
-        .one('transitionend',function(a){
-            $(a.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images>img:nth-child(3)').removeClass('enter').addClass('current');
-}, 4000);
-
-setTimeout(() => {
-    $('.images>img:nth-child(3)').removeClass('current').addClass('leave')
-        .one('transitionend',function(a){
-            $(a.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images>img:nth-child(1)').removeClass('enter').addClass('current');
-}, 6000);
-
-setTimeout(() => {
-    $('.images>img:nth-child(1)').removeClass('current').addClass('leave')
-        .one('transitionend',function(a){
-            $(a.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images>img:nth-child(2)').removeClass('enter').addClass('current');
-}, 8000);
+function x(n){
+    if(n>3){
+        n=n%3
+        if(n===0){
+            n=3
+        }
+    }
+    return n
+}
