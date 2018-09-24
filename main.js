498a16111b710d0261990e16bd521c69bad366ae
Dylan <huanglinjay123@outlook.com>
2018-09-24 20:21:39 +0800

    优化一下代码

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-//初始化位置
-$('.images>img:nth-child(1)').addClass('current');
-$('.images>img:nth-child(2)').addClass('enter');
-$('.images>img:nth-child(3)').addClass('enter');
+let n
+初始化位置()
 
 //每2秒后添加相应类去移动图片
-let n=1
 setInterval(() => {
-    $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend',function(a){
-            $(a.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(a.currentTarget))
         })
-    $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
+    makeCurrent(getImage(n+1))
     n+=1
 }, 2000)
 
@@ -23,3 +20,38 @@ function x(n){
     }
     return n
 }
+
+function 初始化位置(){
+    n=1
+    $(`.images>img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
+}
+
+function makeCurrent($node){
+    return $node.removeClass('leave enter').addClass('current')
+}
+
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
