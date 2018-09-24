d88c6a635a469da897a1e2df60b411c219cb1e0e
Dylan <huanglinjay123@outlook.com>
2018-09-24 17:32:25 +0800

    先无缝轮播一次（思路）

---
+++ main.js
@@ -0,0 +1,37 @@
+
+setTimeout(() => {
+    $('.images>img:nth-child(1)').css({
+        transform:'translateX(-100%)'
+    })
+    $('.images>img:nth-child(2)').css({
+        transform:'translateX(-100%)'
+    })
+    $('.images>img:nth-child(1)').one('transitionend',function(a){
+        $(a.currentTarget).addClass('right').css({transform:'none'})
+    })
+}, 2000);
+
+setTimeout(() => {
+    $('.images>img:nth-child(2)').css({
+        transform:'translateX(-200%)'
+    })
+    $('.images>img:nth-child(3)').css({
+        transform:'translateX(-100%)'
+    })
+    $('.images>img:nth-child(2)').one('transitionend',function(a){
+        $(a.currentTarget).addClass('right').css({transform:'none'})
+    })
+}, 4000);
+
+setTimeout(() => {
+    $('.images>img:nth-child(3)').css({
+        transform:'translateX(-200%)'
+    })
+    $('.images>img:nth-child(1)').css({
+        transform:'translateX(-100%)',
+        'z-index':'1'
+    })
+    $('.images>img:nth-child(3)').one('transitionend',function(a){
+        $(a.currentTarget).addClass('right').css({transform:'none'})
+    })
+}, 6000);  
